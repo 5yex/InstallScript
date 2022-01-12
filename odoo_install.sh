bdaaf8a2cff9159de9a36a53df0aef8a29dbc8f1
5YEX <83497667+5yex@users.noreply.github.com>
2022-01-12 09:26:03 +0100

    Update odoo_install.sh

--- odoo_install.sh
+++ odoo_install.sh
@@ -60,7 +60,6 @@ sudo add-apt-repository universe
 # libpng12-0 dependency for wkhtmltopdf
 sudo add-apt-repository "deb http://mirrors.kernel.org/ubuntu/ xenial main"
 sudo apt-get update
-sudo apt-get upgrade -y
 
 #--------------------------------------------------
 # Install PostgreSQL Server
